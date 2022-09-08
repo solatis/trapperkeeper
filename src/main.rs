2821097bf8d39e353f1bf3950ef89c1ddd26e77d
Leon Mergen <leon@solatis.com>
2022-09-08 13:02:55 +0200

    Templates & cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod web;
 use std::error::Error;
 
 fn run() -> std::result::Result<(), Box<dyn Error>> {
+    let _ = env_logger::builder().try_init();
     let pool = database::PoolBuilder::new().build();
 
     let mut conn = pool.get()?;
