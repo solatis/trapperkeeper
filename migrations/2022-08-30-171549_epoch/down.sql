a583060baac7272e0914fd27e1f37b4f77c0e0ee
Leon Mergen <leon@solatis.com>
2022-09-25 16:11:01 +0200

    Restructure templates

--- migrations/2022-08-30-171549_epoch/down.sql
+++ migrations/2022-08-30-171549_epoch/down.sql
@@ -1,4 +1,4 @@
 -- This file should undo anything in `up.sql`
 
 DROP TABLE IF EXISTS auth_tokens;
-DROP TABLE IF EXISTS apps;
+DROP TABLE IF EXISTS trapps;
--- migrations/2022-08-30-171549_epoch/up.sql
+++ migrations/2022-08-30-171549_epoch/up.sql
@@ -1,13 +1,13 @@
 -- Your SQL goes here
 
-CREATE TABLE apps (
+CREATE TABLE trapps (
        id INTEGER PRIMARY KEY AUTOINCREMENT,
        name TEXT NOT NULL
 );
 
 CREATE TABLE auth_tokens(
        id CHAR(64) PRIMARY KEY NOT NULL,
-       app_id INTEGER NOT NULL,
+       trapp_id INTEGER NOT NULL,
        name TEXT NOT NULL,
-       FOREIGN KEY (app_id) REFERENCES apps(id) ON UPDATE CASCADE ON DELETE CASCADE
+       FOREIGN KEY (trapp_id) REFERENCES trapps(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
